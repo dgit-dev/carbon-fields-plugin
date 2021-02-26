f379db33e13b05ce8da5f4b4e3ef1be83a96dfc5
Darren Griffin <darren.griffin@live.co.uk>
2021-02-26 16:50:18 +0000

    Changed PluginURI

--- carbon-fields-plugin.php
+++ carbon-fields-plugin.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: Carbon Fields
  * Description: WordPress developer-friendly custom fields for post types, taxonomy terms, users, comments, widgets, options, navigation menus and more.
- * Version: 3.2.2
+ * Version: 3.2.2-dgit-1
  * Author: htmlburger
  * Author URI: https://htmlburger.com/
- * Plugin URI: http://carbonfields.net/
+ * Plugin URI: https://github.com/dgit-dev/carbon-fields-plugin
  * License: GPL2
  * Requires at least: 4.0
  * Tested up to: 5.5.3
